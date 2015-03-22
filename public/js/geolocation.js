84ba87b89032afa6ca9ac546b420698bb5f866af
kingdido999 <kingdido999@gmail.com>
2015-03-22 19:02:38 -0400

    enabled geolocation

---
+++ public/js/geolocation.js
@@ -0,0 +1,40 @@
+/*
+ * Modified based on the following examples:
+ * Geolocation:
+ * https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
+ */
+
+function initialize() {
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            // get current location
+            var pos = new google.maps.LatLng(
+                position.coords.latitude,
+                position.coords.longitude
+            );
+
+            // get address input element
+            var address = document.getElementsByClassName('start-address')[0];
+
+            // change address input value to current address
+            address.value = pos;
+        }, function() {
+            // geolocation failed
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+}
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        alert('Error: The Geolocation service failed.');
+    } else {
+        alert('Error: Your browser doesn\'t support geolocation.');
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
