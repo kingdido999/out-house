e5bb2b6245103acb0a30e8de0bfefe6bf3dcde92
kingdido999 <kingdido999@gmail.com>
2015-04-27 16:30:50 -0400

    remove email from user model

--- models/user.js
+++ models/user.js
@@ -3,9 +3,8 @@ var mongoose = require('mongoose'),
     passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: { type: String, unique: true },
-    email: { type: String, unique: true },
-    password: String,
+    username: { type: String, unique: true, required: true },
+    password: { type: String, require: true },
     gender: String,
     age: Number
 });
